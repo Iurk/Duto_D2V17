d4c2f87f0a649cfdbb18e161e82c50b13a98cf35
Matheus <matheusamplatz@gmail.com>
2021-01-13 18:23:31 -0300

    Att
    Novas malhas criadas
    
    * inlet
    * outlet
    * walls

--- python/mesh.py
+++ python/mesh.py
@@ -10,24 +10,41 @@ import numpy as np
 import utilidades as utils
 
 Sim_yaml = "./bin/dados.yml"
+path_mesh = './bin/Mesh/'
+
 with open(Sim_yaml) as file:
     simulation = yaml.load(file, Loader=yaml.FullLoader)
 
 Nx = simulation['domain']['Nx']
 Ny = simulation['domain']['Ny']
 
-solid = np.zeros((Ny, Nx), dtype=bool)
-solid[0,:] = True
-solid[Ny-1,:] = True
+walls = np.zeros((Ny, Nx), dtype=bool)
+inlet = np.zeros_like(walls)
+outlet = np.zeros_like(walls)
 
-solid = solid.flatten()
+walls[0,:] = True
+walls[Ny-1,:] = True
+
+inlet[:, 0:3] = True
+# inlet[:, 0] = True
+
+start = Nx-3
+outlet[:, start:Nx] = True
+# outlet[:, Nx-1] = True
+
+walls = walls.flatten()
+inlet = inlet.flatten()
+outlet = outlet.flatten()
 
 pasta = utils.criar_pasta("Mesh", main_root="./bin")
 
-name_solid = "mesh.bin"
+mesh_data = [walls, inlet, outlet]
+mesh_files = ['walls.bin', 'inlet.bin', 'outlet.bin']
 
-solid_path = pasta + "/%s" % name_solid
-with open(solid_path, 'wb') as file:
-    file.write(bytearray(solid))
+
+for i in range(len(mesh_data)):
+    file_path = path_mesh + mesh_files[i]
+    with open(file_path, 'wb') as file:
+        file.write(bytearray(mesh_data[i]))
 
 
