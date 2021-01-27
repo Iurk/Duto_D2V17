6b13caacc45e9ee3e3c22ba1ff5db5352150e53d
Matheus <matheusamplatz@gmail.com>
2021-01-27 15:45:19 -0300

    Att
    Adicionado o plot do perfil de velocidade

--- python/data_process.py
+++ python/data_process.py
@@ -14,11 +14,16 @@ import multiprocessing as mp
 import funcoes_graficos as fg
 from time import time
 
-def plotting(args):
+def plotting_image(args):
     idx_file, rho, ux, uy = args
     u_mod = np.sqrt(ux**2 + uy**2)
     
     fg.image(u_mod, idx_file, pasta_img)
+    
+def plotting_perfil(args):
+    idx_file, ux, y = args
+    
+    fg.grafico(ux[:, Point], y, idx_file, pasta_perfil)
     
 if __name__ == '__main__':
 
@@ -33,6 +38,7 @@ if __name__ == '__main__':
     
     Nx = data['domain']['Nx']
     Ny = data['domain']['Ny']
+    Point = int(Nx/2)
     
     Steps = data['simulation']['NSTEPS']
     Saves = data['simulation']['NSAVE']
@@ -40,7 +46,7 @@ if __name__ == '__main__':
     
     results = "./bin/Results/"
     pasta_img = util.criar_pasta('Images', folder=velocity, main_root=main)
-    # pasta_stream = util.criar_pasta('Stream', folder=velocity, main_root=main)
+    pasta_perfil = util.criar_pasta('Perfil', folder=velocity, main_root=main)
     
     rho_files = []
     ux_files = []
@@ -56,12 +62,12 @@ if __name__ == '__main__':
                 dic[var].append(path_full)
                 
     x = np.arange(1, Nx+1, 1)
-    y = np.arange(1, Ny+1, 1)
     
     CPU = mp.cpu_count()
     pool = mp.Pool()
     
     idx = []
+    ys = np.empty((CPU, Ny))
     rhos = np.empty((CPU, Ny, Nx))
     uxs = np.empty_like(rhos)
     uys = np.empty_like(rhos)
@@ -76,18 +82,21 @@ if __name__ == '__main__':
             rhos[j] = np.fromfile(rho_files[i], dtype='float64').reshape(Ny, Nx)
             uxs[j] = np.fromfile(ux_files[i], dtype='float64').reshape(Ny, Nx)
             uys[j] = np.fromfile(uy_files[i], dtype='float64').reshape(Ny, Nx)
+            ys[j] = np.arange(1, Ny+1, 1)
             
             i += 1
             if(i == len(rho_files)):
                 break
         
-        inputs = zip(idx, rhos, uxs, uys)
-        pool.map(plotting, inputs)
+        image_inputs = zip(idx, rhos, uxs, uys)
+        perfil_inputs = zip(idx, uxs, ys)
+        pool.map(plotting_image, image_inputs)
+        pool.map(plotting_perfil, perfil_inputs)
         idx = []
         
     print('Animating...')
-    fg.animation('Velocidade', './', pasta_img)
-    # fg.animation('Stream', './', pasta_stream)
+    fg.animation('Velocidade', main, pasta_img)
+    fg.animation('Perfil', main, pasta_perfil)
     print('Done!')
     fim = time()
     print("Finish in {} s".format(fim - ini))
