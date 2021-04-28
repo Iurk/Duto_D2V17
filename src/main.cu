2c12ed1c491e4e8a62e49a1d65bafb627763322a
Matheus <matheusamplatz@gmail.com>
2021-04-28 19:43:39 -0300

    Att
    Adição do bounce-back

--- src/main.cu
+++ src/main.cu
@@ -6,6 +6,7 @@
 #include "LBM.h"
 #include "dados.h"
 #include "saving.h"
+#include "boundary.h"
 
 using namespace myGlobals;
 
@@ -151,6 +152,7 @@ int main(int argc, char const *argv[]){
 		}
 */
 		stream_collide_save(f1_gpu, f2_gpu, feq_gpu, frec_gpu, S_gpu, rho_gpu, ux_gpu, uy_gpu, need_scalars);
+		bounce_back(f2_gpu);
 
 		if(save){
 			save_scalar("rho",rho_gpu, scalar_host, n+1);
