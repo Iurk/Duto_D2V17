6a8a30abf40e33b73d5b2712b93a779f24102204
Matheus <matheusamplatz@gmail.com>
2021-05-26 20:56:03 -0300

    Att
    Prints de verificação

--- src/main.cu
+++ src/main.cu
@@ -158,8 +158,10 @@ int main(int argc, char const *argv[]){
 */
 		stream_collide_save(f1_gpu, f2_gpu, feq_gpu, fneq_gpu, frec_gpu, S_gpu, rho_gpu, ux_gpu, uy_gpu, txx_gpu, txy_gpu, tyy_gpu, need_scalars);
 		inlet_BC(1.05, 0.04, f2_gpu, feq_gpu, frec_gpu, rho_gpu, ux_gpu, uy_gpu, txx_gpu, txy_gpu, tyy_gpu, "PP");
-		outlet_BC(1.0, 0.04, f2_gpu, feq_gpu, frec_gpu, rho_gpu, ux_gpu, uy_gpu, txx_gpu, txy_gpu, tyy_gpu, "PP");
+		printf("Init Outlet\n");
+		outlet_BC(1.0, 0.04, f2_gpu, feq_gpu, frec_gpu, rho_gpu, ux_gpu, uy_gpu, txx_gpu, txy_gpu, tyy_gpu, "FDP");
 		//bounce_back(f2_gpu);
+		//printf("Initi Wall\n");
 		wall_velocity(f2_gpu, feq_gpu, frec_gpu, rho_gpu, ux_gpu, uy_gpu, txx_gpu, txy_gpu, tyy_gpu);
 		//corners(f2_gpu, frec_gpu, rho_gpu, ux_gpu, uy_gpu, txx_gpu, txy_gpu, tyy_gpu);
 
