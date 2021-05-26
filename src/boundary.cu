d07b000458f9cb00755a41262bb3d1a03210d5b4
Matheus <matheusamplatz@gmail.com>
2021-05-26 20:56:23 -0300

    Att
    Resolução da condição de pressão prescrita pelo host

--- src/boundary.cu
+++ src/boundary.cu
@@ -1,16 +1,39 @@
 #include "LBM.h"
 #include "dados.h"
 #include "boundary.h"
+#include "system_solve.h"
 
 using namespace myGlobals;
 
-__device__ __forceinline__ size_t gpu_scalar_index(unsigned int x, unsigned int y){
-	return Nx_d*y + x;
+__host__ __device__ __forceinline__ size_t gpu_scalar_index(unsigned int x, unsigned int y){
+	#if defined(__CUDA_ARCH__)
+		return Nx_d*y + x;
+	#else
+		return Nx*y + x;
+	#endif
 }
 
-__device__ __forceinline__ size_t gpu_fieldn_index(unsigned int x, unsigned int y, unsigned int d){
-	return (Nx_d*(Ny_d*(d) + y) + x);
+__host__ __device__ __forceinline__ size_t gpu_fieldn_index(unsigned int x, unsigned int y, unsigned int d){
+	#if defined(__CUDA_ARCH__)
+		return Nx_d*(Ny_d*(d) + y) + x;
+	#else
+		return Nx*(Ny*(d) + y) + x;
+	#endif
 }
+
+typedef struct{
+	unsigned int x, y, NI;
+	unsigned int *I, *IN;
+	double *f;
+	double rho, uy;
+}*InputData;
+
+__host__ int func(N_Vector, N_Vector, void*);
+__host__ void host_inlet_PP(double, double*, double*);
+__host__ double host_recursive_PP(unsigned int, unsigned int, unsigned int, double, double, double, double, double, double);
+__host__ void host_hermite_polynomial(int, int, double, double*);
+__host__ void host_hermite_moments(double, double, double, double, double, double, double*);
+__host__ void host_recursive(unsigned int, unsigned int, double, double, double, double, double, double, double*);
 
 __global__ void gpu_inlet(double, double, double*, double*, double*, double*, double*, double*, double*, double*, double*, unsigned int);
 __global__ void gpu_bounce_back(double*);
@@ -241,7 +264,7 @@ __device__ void device_inlet_VP(unsigned int x, unsigned int y, double ux_in, do
 		txy[gpu_scalar_index(x, y)] = tauxy;
 		tyy[gpu_scalar_index(x, y)] = tauyy;
 
-		gpu_recursive(x, y, rho, ux_in, uy_in, tauxx, tauxy, tauyy, frec);
+		recursive_dist(x, y, rho, ux_in, uy_in, tauxx, tauxy, tauyy, frec);
 		for(int n = 0; n < q; ++n){
 			f[gpu_fieldn_index(x, y, n)] = frec[gpu_fieldn_index(x, y, n)];
 		}
@@ -337,6 +360,40 @@ __device__ void device_inlet_PP(unsigned int x, unsigned int y, double rho_in, d
 	}
 }
 
+__device__ void device_interpolation(unsigned int x, unsigned int y, double *f, double *feq){
+
+	printf("x: %d, y: %d\n", x, y);
+	if(x == 0){
+		if(y == 5){
+			printf("f\n");
+			printf("f0: %g f1: %g f2: %g\n", f[gpu_fieldn_index(x, y, 0)], f[gpu_fieldn_index(x, y, 1)], f[gpu_fieldn_index(x, y, 2)]);
+			printf("f3: %g f4: %g f5: %g\n", f[gpu_fieldn_index(x, y, 3)], f[gpu_fieldn_index(x, y, 4)], f[gpu_fieldn_index(x, y, 5)]);
+			printf("f6: %g f7: %g f8: %g\n", f[gpu_fieldn_index(x, y, 6)], f[gpu_fieldn_index(x, y, 7)], f[gpu_fieldn_index(x, y, 8)]);
+			printf("f9: %g f10: %g f11: %g\n", f[gpu_fieldn_index(x, y, 9)], f[gpu_fieldn_index(x, y, 10)], f[gpu_fieldn_index(x, y, 11)]);
+			printf("f12: %g f13: %g f14: %g\n", f[gpu_fieldn_index(x, y, 12)], f[gpu_fieldn_index(x, y, 13)], f[gpu_fieldn_index(x, y, 14)]);
+			printf("f15: %g f16: %g\n", f[gpu_fieldn_index(x, y, 15)], f[gpu_fieldn_index(x, y, 16)]);
+		}
+	}
+	if(x == 1){
+		f[gpu_fieldn_index(x, y, 9)] = f[gpu_fieldn_index(x, y, 11)] - feq[gpu_fieldn_index(x, y, 11)] + feq[gpu_fieldn_index(x, y, 9)];
+		f[gpu_fieldn_index(x, y, 12)] = f[gpu_fieldn_index(x, y, 10)] - feq[gpu_fieldn_index(x, y, 10)] + feq[gpu_fieldn_index(x, y, 12)];
+
+		if(y > 0 && y < Ny_d-2){
+			f[gpu_fieldn_index(x, y, 9)] = (1.0/3.0)*f[gpu_fieldn_index(x-1, y-1, 9)] + f[gpu_fieldn_index(x+1, y+1, 9)] - (1.0/3.0)*f[gpu_fieldn_index(x+2, y+2, 9)];
+		}
+
+		if(y > 1 && y < Ny_d-1){
+			f[gpu_fieldn_index(x, y, 12)] = (1.0/3.0)*f[gpu_fieldn_index(x-1, y+1, 12)] + f[gpu_fieldn_index(x+1, y-1, 12)] - (1.0/3.0)*f[gpu_fieldn_index(x+2, y-2, 12)];
+		}
+		
+		f[gpu_fieldn_index(x, y, 13)] = (1.0/2.0)*f[gpu_fieldn_index(x-1, y, 13)] + f[gpu_fieldn_index(x+2, y, 13)] - (1.0/2.0)*f[gpu_fieldn_index(x+3, y, 13)];
+	}
+
+	else if(x == 2){
+		f[gpu_fieldn_index(x, y, 13)] = (1.0/6.0)*f[gpu_fieldn_index(x-2, y, 13)] + (4.0/3.0)*f[gpu_fieldn_index(x+1, y, 13)] - (1.0/2.0)*f[gpu_fieldn_index(x+2, y, 13)];
+	}
+}
+
 __device__ void device_outlet_VP(unsigned int x, unsigned int y, double ux_out, double *f, double *feq, double *frec, double *r, double *u, double *v, double*txx, double *txy, double *tyy){
 
 	double uy_out = 0.0;
@@ -371,7 +428,7 @@ __device__ void device_outlet_VP(unsigned int x, unsigned int y, double ux_out, 
 		txy[gpu_scalar_index(x, y)] = tauxy;
 		tyy[gpu_scalar_index(x, y)] = tauyy;
 
-		gpu_recursive(x, y, rho, ux_out, uy_out, tauxx, tauxy, tauyy, frec);
+		recursive_dist(x, y, rho, ux_out, uy_out, tauxx, tauxy, tauyy, frec);
 		for(int n = 0; n < q; ++n){
 			f[gpu_fieldn_index(x, y, n)] = frec[gpu_fieldn_index(x, y, n)];
 		}
@@ -518,7 +575,7 @@ __device__ void device_wall_velocity(unsigned int x, unsigned int y, double *f, 
 		txy[gpu_scalar_index(x, y)] = tauxy;
 		tyy[gpu_scalar_index(x, y)] = tauyy;
 
-		gpu_recursive(x, y, rho, ux, uy, tauxx, tauxy, tauyy, frec);
+		recursive_dist(x, y, rho, ux, uy, tauxx, tauxy, tauyy, frec);
 		for(int n = 0; n < q; ++n){
 			f[gpu_fieldn_index(x, y, n)] = frec[gpu_fieldn_index(x, y, n)];
 		}
@@ -563,7 +620,7 @@ __device__ void device_wall_velocity(unsigned int x, unsigned int y, double *f, 
 		txy[gpu_scalar_index(x, y)] = tauxy;
 		tyy[gpu_scalar_index(x, y)] = tauyy;
 
-		gpu_recursive(x, y, rho, ux, uy, tauxx, tauxy, tauyy, frec);
+		recursive_dist(x, y, rho, ux, uy, tauxx, tauxy, tauyy, frec);
 		for(int n = 0; n < q; ++n){
 			f[gpu_fieldn_index(x, y, n)] = frec[gpu_fieldn_index(x, y, n)];
 		}
@@ -601,7 +658,7 @@ __device__ void device_corners_first(unsigned int x, unsigned int y, unsigned in
 	rhoI = *prhoI;
 	double rho = 1.4971916997958*rhoI;
 
-	gpu_recursive(x, y, rho, ux, uy, tauxx, tauxy, tauyy, frec);
+	recursive_dist(x, y, rho, ux, uy, tauxx, tauxy, tauyy, frec);
 
 	for(int n = 0; n < q; ++n){
 		f[gpu_fieldn_index(x, y, n)] = frec[gpu_fieldn_index(x, y, n)];
@@ -699,7 +756,7 @@ __global__ void gpu_bounce_back(double *f){
 	}
 }
 
-__host__ void inlet_BC(double rho_in, double ux_in, double *f, double *feq, double *frec, double *r, double *u, double *v, double *txx, double *txy, double *tyy, std::string mode){
+__host__ void inlet_BC(double *solution, double rho_in, double ux_in, double *f, double *feq, double *frec, double *r, double *u, double *v, double *txx, double *txy, double *tyy, std::string mode){
 
 	dim3 grid(Nx/nThreads, Ny, 1);
 	dim3 block(nThreads, 1, 1);
@@ -710,10 +767,64 @@ __host__ void inlet_BC(double rho_in, double ux_in, double *f, double *feq, doub
 	}
 	else if(mode == "PP"){
 		mode_num = 2;
+		host_inlet_PP(rho_in, solution, f);
 	}
 
+	printf("Hi before calling the kernel\n");
+	printf("mode_num: %d\n", mode_num);
 	gpu_inlet<<< grid, block >>>(rho_in, ux_in, f, feq, frec, r, u, v, txx, txy, tyy, mode_num);
 	getLastCudaError("gpu_inlet kernel error");
+}
+
+__host__ void host_inlet_PP(double rho_in, double *solution, double *f_gpu){
+
+	double uy_in = 0.0;
+
+	double guess[NVAR] = {0.0, 0.0, 0.0, 0.0};
+
+	unsigned int NI = 11;
+	unsigned int I[11] = {0, 2, 3, 4, 6, 7, 10, 11, 14, 15, 16};
+	unsigned int IN[6] = {1, 5, 8, 9, 12, 13};
+
+	double *f_pinned;
+	checkCudaErrors(cudaMallocHost((void**)&f_pinned, mem_size_ndir));
+	checkCudaErrors(cudaMemcpy(f_pinned, f_gpu, mem_size_ndir, cudaMemcpyDeviceToHost));
+
+	InputData input = NULL;
+	input = (InputData)malloc(sizeof *input);
+	input->NI = NI;
+	input->I = I;
+	input->IN = IN;
+	input->f = f_pinned;
+	input->rho = rho_in;
+	input->uy = uy_in;
+
+	for(int y = 0; y < Ny; ++y){
+		int x = 0;
+
+		input->x = x;
+		input->y = y;
+		solving(guess, solution, input, func);
+
+		double ux = solution[0];
+		double tauxx = solution[1];
+		double tauxy = solution[2];
+		double tauyy = solution[3];
+		host_recursive(x, y, rho_in, ux, uy_in, tauxx, tauxy, tauyy, f_pinned);
+
+		if(y == 5){
+			printf("f_pinned\n");
+			printf("f0: %g f1: %g f2: %g\n", f_pinned[gpu_fieldn_index(x, y, 0)], f_pinned[gpu_fieldn_index(x, y, 1)], f_pinned[gpu_fieldn_index(x, y, 2)]);
+			printf("f3: %g f4: %g f5: %g\n", f_pinned[gpu_fieldn_index(x, y, 3)], f_pinned[gpu_fieldn_index(x, y, 4)], f_pinned[gpu_fieldn_index(x, y, 5)]);
+			printf("f6: %g f7: %g f8: %g\n", f_pinned[gpu_fieldn_index(x, y, 6)], f_pinned[gpu_fieldn_index(x, y, 7)], f_pinned[gpu_fieldn_index(x, y, 8)]);
+			printf("f9: %g f10: %g f11: %g\n", f_pinned[gpu_fieldn_index(x, y, 9)], f_pinned[gpu_fieldn_index(x, y, 10)], f_pinned[gpu_fieldn_index(x, y, 11)]);
+			printf("f12: %g f13: %g f14: %g\n", f_pinned[gpu_fieldn_index(x, y, 12)], f_pinned[gpu_fieldn_index(x, y, 13)], f_pinned[gpu_fieldn_index(x, y, 14)]);
+			printf("f15: %g f16: %g\n", f_pinned[gpu_fieldn_index(x, y, 15)], f_pinned[gpu_fieldn_index(x, y, 16)]);
+		}
+	}
+
+	checkCudaErrors(cudaMemcpy(f_gpu, f_pinned, mem_size_ndir, cudaMemcpyHostToDevice));
+	printf("Copied\n");
 }
 
 __global__ void gpu_inlet(double rho_in, double ux_in, double *f, double *feq, double *frec, double *r, double *u, double *v, double *txx, double *txy, double *tyy, unsigned int mode_num){
@@ -721,13 +832,16 @@ __global__ void gpu_inlet(double rho_in, double ux_in, double *f, double *feq, d
 	unsigned int y = blockIdx.y;
 	unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
 
+	printf("Hi from inside the kernel\n");
+
 	bool node_inlet = inlet_d[gpu_scalar_index(x, y)];
 	if(node_inlet){
 		if(mode_num == 1){
 			device_inlet_VP(x, y, ux_in, f, feq, frec, r, u, v, txx, txy, tyy);
 		}
 		else if(mode_num == 2){
-			device_inlet_PP(x, y, rho_in, f, feq, frec, r, u, v, txx, txy, tyy);
+			//device_inlet_PP(x, y, rho_in, f, feq, frec, r, u, v, txx, txy, tyy);
+			device_interpolation(x, y, f, feq);
 		}
 	}
 
@@ -866,4 +980,126 @@ __global__ void gpu_corners(double *f, double *frec, double *r, double *u, doubl
 
 	att_moments(x, y, f, r, u, v, txx, txy, tyy);
 
+}
+
+__host__ int func(N_Vector u, N_Vector f, void *user_data){
+
+	realtype *udata, *fdata;
+	realtype ux, tauxx, tauxy, tauyy;
+
+	unsigned int x, y, NI;
+	unsigned int *I, *IN;
+	double *frec;
+	double rho, uy;
+
+	InputData input = (InputData)user_data;
+
+	x = input->x;
+	y = input->y;
+	NI = input->NI;
+	I = input->I;
+	IN = input->IN;
+	frec = input->f;
+	rho = input->rho;
+	uy = input->uy;
+
+	udata = N_VGetArrayPointer(u);
+	fdata = N_VGetArrayPointer(f);
+
+	ux = udata[0];
+	tauxx = udata[1];
+	tauxy = udata[2];
+	tauyy = udata[3];
+
+	double cs = 1.0/as;
+	double cs2 = cs*cs;
+
+	double rhoIax = 0.0, rhoIaxx = 0.0, rhoIaxy = 0.0, rhoIayy = 0.0;
+	for(int n = 0; n < NI; ++n){
+		unsigned int ni = I[n];
+
+		double ex2 = ex[ni]*ex[ni];
+		double ey2 = ey[ni]*ey[ni];
+
+		rhoIax += frec[gpu_fieldn_index(x, y, ni)]*ex[ni];
+		rhoIaxx += frec[gpu_fieldn_index(x, y, ni)]*(ex2 - cs2);
+		rhoIaxy += frec[gpu_fieldn_index(x, y, ni)]*ex[ni]*ey[ni];
+		rhoIayy += frec[gpu_fieldn_index(x, y, ni)]*(ey2 - cs2);
+	}
+
+	double rhoINax = 0.0, rhoINaxx = 0.0, rhoINaxy = 0.0, rhoINayy = 0.0;
+	for(int n = 0; n < ndir-NI; ++n){
+		unsigned int ni = IN[n];
+
+		double ex2 = ex[ni]*ex[ni];
+		double ey2 = ey[ni]*ey[ni];
+
+		rhoINax += host_recursive_PP(ni, x, y, rho, ux, uy, tauxx, tauxy, tauyy)*ex[ni];
+		rhoINaxx += host_recursive_PP(ni, x, y, rho, ux, uy, tauxx, tauxy, tauyy)*(ex2 - cs2);
+		rhoINaxy += host_recursive_PP(ni, x, y, rho, ux, uy, tauxx, tauxy, tauyy)*ex[ni]*ey[ni];
+		rhoINayy += host_recursive_PP(ni, x, y, rho, ux, uy, tauxx, tauxy, tauyy)*(ey2 - cs2);
+	}
+
+	fdata[0] = rhoIax + rhoINax - rho*ux;
+	fdata[1] = rhoIaxx + rhoINaxx - rho*ux*ux - tauxx;
+	fdata[2] = rhoIaxy + rhoINaxy - rho*ux*uy - tauxy;
+	fdata[3] = rhoIayy + rhoIayy - rho*uy*uy - tauyy;
+
+	return (0);
+}
+
+__host__ double host_recursive_PP(unsigned int n, unsigned int x, unsigned int y, double rho, double ux, double uy, double tauxx, double tauxy, double tauyy){
+
+	double frec;
+
+	double cs = 1.0/as;
+	double cs2 = cs*cs;
+	double cs4 = cs2*cs2;
+	double cs6 = cs4*cs2;
+
+	double A = 1.0/(cs2);
+	double B = 1.0/(2.0*cs4);
+	double C = 1.0/(6.0*cs6);
+
+	double W[] = {w0, wp, wp, wp, wp, ws, ws, ws, ws, wt, wt, wt, wt, wq, wq, wq, wq};
+
+	// Calculating the regularized recursive distribution
+	double a[10], H[10];
+	hermite_polynomial(ex[n], ey[n], cs, H);
+	hermite_moments(rho, ux, uy, tauxx, tauxy, tauyy, a);
+
+	double order_1 = A*(a[1]*H[1] + a[2]*H[2]);
+	double order_2 = B*(a[3]*H[3] + 2*a[4]*H[4] + a[5]*H[5]);
+	double order_3 = C*(a[6]*H[6] + 3*a[7]*H[7] + 3*a[8]*H[8] + a[9]*H[9]);
+
+	frec = W[n]*(a[0]*H[0] + order_1 + order_2 + order_3);
+	return frec;
+}
+
+// Recursive Regularized Distribuition
+__host__ void host_recursive(unsigned int x, unsigned int y, double rho, double ux, double uy, double tauxx, double tauxy, double tauyy, double *frec){
+
+	double cs = 1.0/as;
+	double cs2 = cs*cs;
+	double cs4 = cs2*cs2;
+	double cs6 = cs4*cs2;
+
+	double A = 1.0/(cs2);
+	double B = 1.0/(2.0*cs4);
+	double C = 1.0/(6.0*cs6);
+
+	double W[] = {w0, wp, wp, wp, wp, ws, ws, ws, ws, wt, wt, wt, wt, wq, wq, wq, wq};
+
+	// Calculating the regularized recursive distribution
+	double a[10], H[10];
+	for(int n = 0; n < ndir; ++n){
+		hermite_polynomial(ex[n], ey[n], cs, H);
+		hermite_moments(rho, ux, uy, tauxx, tauxy, tauyy, a);
+
+		double order_1 = A*(a[1]*H[1] + a[2]*H[2]);
+		double order_2 = B*(a[3]*H[3] + 2*a[4]*H[4] + a[5]*H[5]);
+		double order_3 = C*(a[6]*H[6] + 3*a[7]*H[7] + 3*a[8]*H[8] + a[9]*H[9]);
+
+		frec[gpu_fieldn_index(x, y, n)] = W[n]*(a[0]*H[0] + order_1 + order_2 + order_3);
+	}
 }
